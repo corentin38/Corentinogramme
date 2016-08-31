94404fe4bc219bce6113992950b33f455103b5a8
Corentin Marciau <corentin@marciau.fr>
2016-09-01 01:14:59 +0200

    [DEV] Working state with custom assets

--- app/src/main/java/moi/fr/corentinogramme/TileFragment.java
+++ app/src/main/java/moi/fr/corentinogramme/TileFragment.java
@@ -1,18 +1,78 @@
 package moi.fr.corentinogramme;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.item_tile, null);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        // Set padding for Tiles
+        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        return recyclerView;
     }
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public ImageView picture;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_tile, parent, false));
+            picture = (ImageView) itemView.findViewById(R.id.tile_picture);
+            title = (TextView) itemView.findViewById(R.id.tile_title);
+        }
+    }
+
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 6;
+        private final String[] mTitles;
+        private final Drawable[] mXpPictures;
+
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mTitles = resources.getStringArray(R.array.experiences);
+            TypedArray a = resources.obtainTypedArray(R.array.experiences_avator);
+            mXpPictures = new Drawable[a.length()];
+            for (int i = 0; i < mXpPictures.length; i++) {
+                mXpPictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.picture.setImageDrawable(mXpPictures[position % mXpPictures.length]);
+            holder.title.setText(mTitles[position % mTitles.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
+    }
 }
